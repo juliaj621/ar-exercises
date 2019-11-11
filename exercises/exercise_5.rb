144f1f2e84a475a3ddb7fcc4628cab96bb924f4b
Julia Jachimowicz <juliaj621@gmail.com>
2019-11-11 15:47:54 -0800

    fetched revenue calculations

--- exercises/exercise_5.rb
+++ exercises/exercise_5.rb
@@ -8,3 +8,13 @@ puts "Exercise 5"
 puts "----------"
 
 # Your code goes here ...
+
+@total_revenue = Store.sum(:annual_revenue)
+
+puts @total_revenue
+
+@average_revenue = Store.average(:annual_revenue)
+
+puts @average_revenue
+
+puts Store.where(Store.arel_table[:annual_revenue].gt(1000000)).count
