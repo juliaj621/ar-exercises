ba13489695e73026878b9252884f97fe0e677c0b
Julia Jachimowicz <juliaj621@gmail.com>
2019-11-11 16:50:27 -0800

    validated stores/employees and validation message

--- exercises/exercise_7.rb
+++ exercises/exercise_7.rb
@@ -10,3 +10,15 @@ puts "Exercise 7"
 puts "----------"
 
 # Your code goes here ...
+
+puts "Input name of a store here:"
+input = gets.chomp
+
+store = Store.create(name: input)
+
+puts "Wait a second..."
+
+# puts "Annual revenue #{store.errors[:annual_revenue]}"
+# puts "Women's or men's apparel #{store.errors[:men_or_women]}"
+
+store.errors.each {|error| puts "You cannot leave the following blank: #{error}"}
--- lib/store.rb
+++ lib/store.rb
@@ -1,3 +1,13 @@
 class Store < ActiveRecord::Base
   has_many :employees
+  validates :name, presence: true, length: {minimum: 3}
+  validates :annual_revenue, presence: true, numericality: {greater_than_or_equal_to: 0}
+  validates :men_or_women, presence: true
+
+  def men_or_women
+    if mens_apparel == true || womens_apparel == true
+      true
+    end
+  end
+
 end
